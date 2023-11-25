242f2c58e42371326c0a4389229d9d1299a85617
marcoscabralg <mcabral@globons.com>
2023-11-25 10:32:12 -0300

    pwa test

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,19 @@ import App from './App.tsx'
 import './index.css'
 import "nes.css/css/nes.min.css";
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
+  
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "PokeDale",
+        short_name: "PDL",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
+  ],
+});
